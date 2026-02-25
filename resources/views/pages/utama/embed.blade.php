846e164d425e678ac1d1183c97d87f3bdcb93bcd
hazimah-uitm <hazimahpethie@uitm.edu.my>
2026-02-25 14:36:34 +0800

    updated embed

--- resources/views/pages/utama/embed.blade.php
+++ resources/views/pages/utama/embed.blade.php
@@ -118,8 +118,9 @@
 </head>
 
 <body>
-    <div class="container py-2">
+    <div class="container py-2 mt-4">
         <div id="publicCalendar"></div>
+        <small class="text-muted fst-italic">*Sila klik program untuk maklumat lanjut. Tarikh dan masa program tertakluk pada pindaan.</small>
     </div>
 
     <!-- Modal: Maklumat Program -->
